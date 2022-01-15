76dc4e7d6827449c603730e6231025dd619d849c
Bernard Adhitya <bernard.adhitya@gmail.com>
2022-01-15 22:06:33 +0700

    Create endpoints to access main service

--- src/env.js
+++ src/env.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyChUhpJu7f7ifxZ69MUr6eQIZPWTBmAmfg",
-  authDomain: "kitchen-price-service.firebaseapp.com",
-  projectId: "kitchen-price-service",
-  storageBucket: "kitchen-price-service.appspot.com",
-  messagingSenderId: "969863311330",
-  appId: "1:969863311330:web:f7b1d3bb204f35104353bb"
-}
+  apiKey: "AIzaSyAooxkjFM16qcoVyYjmYfHCNwpTcRg-Cvk",
+  authDomain: "good-gardener-service.firebaseapp.com",
+  projectId: "good-gardener-service",
+  storageBucket: "good-gardener-service.appspot.com",
+  messagingSenderId: "787285447030",
+  appId: "1:787285447030:web:a35fc3e90c4917efe8aa57"
+};
 
 const supabaseConfig = {
-  url: 'https://qjukedyogppqdaqyvbic.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjIzMTM1MTAzLCJleHAiOjE5Mzg3MTExMDN9.47AxP5lb7ijpaC6p006psBBv8NmYdb2jYmM7Zj8c35U'
+  url: 'https://kxidlyhczmlcdewwvhjc.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJzZXJ2aWNlX3JvbGUiLCJpYXQiOjE2NDIyMzMwNTUsImV4cCI6MTk1NzgwOTA1NX0.ZG46R6K9WtIaGocCEUMllxO0sfs3kcxgYl5zW7SmYXI'
 }
 
 const newsApiConfig = {
