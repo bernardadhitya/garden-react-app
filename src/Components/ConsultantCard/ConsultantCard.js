0ed3e2ab9616766c0cdb0fa59185af4d11caf7f5
Bernard Adhitya <bernard.adhitya@gmail.com>
2022-01-15 10:43:25 +0700

    First commit

---
+++ src/Components/ConsultantCard/ConsultantCard.js
@@ -0,0 +1,53 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import StarIcon from '@material-ui/icons/Star';
+import './ConsultantCard.css';
+import { useHistory } from 'react-router-dom';
+import { addUserSearchHistory } from '../../firebase';
+
+const ConsultantCard = (props) => {
+  const {
+    image,
+    name,
+    rating,
+    productId
+  } = props;
+
+  const history = useHistory();
+
+  const handleClick = async () => {
+    await addUserSearchHistory(productId);
+    history.push(`/${productId}`);
+  }
+
+  return (
+    <Grid item xs={3}>
+      <div className='consultant-card' onClick={() => handleClick()}>
+        <Grid container>
+          <Grid xs={1}/>
+          <Grid xs={4}>
+            <div className='consultant-image'>
+              <img
+                src={image || require('../../Assets/images/logo-bw.png')}
+                className={image ? 'image-thumbnail' : 'image-thumbnail-empty'}
+                alt=''
+              />
+            </div>
+          </Grid>
+          <Grid xs={6}>
+            <div className='consultant-content'>
+              <div className='consultant-name'>{name}</div>
+              <div className='consultant-rating'>
+                <StarIcon style={{color: '#FFC107'}}/>
+                {`${rating}/5`}
+              </div>
+            </div>
+          </Grid>
+          <Grid xs={1}/>
+        </Grid>
+      </div>
+    </Grid>
+  )
+}
+
+export default ConsultantCard;
---
+++ src/Components/ItemCard/ItemCard.js
@@ -0,0 +1,42 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import { formattedCurrency } from '../../Constants/format';
+import './ItemCard.css';
+import { useHistory } from 'react-router-dom';
+import { addUserSearchHistory } from '../../firebase';
+
+const ItemCard = (props) => {
+  const {
+    image,
+    title,
+    price,
+    productId
+  } = props;
+
+  const history = useHistory();
+
+  const handleClick = async () => {
+    await addUserSearchHistory(productId);
+    history.push(`/${productId}`);
+  }
+
+  return (
+    <Grid item xs={3}>
+      <div className='item-card' onClick={() => handleClick()}>
+        <div className='item-image'>
+          <img
+            src={image || require('../../Assets/images/logo-bw.png')}
+            className={image ? 'image-thumbnail' : 'image-thumbnail-empty'}
+            alt=''
+          />
+        </div>
+        <div className='item-content'>
+          <div className='item-title'>{title}</div>
+          <div className='item-price'>{formattedCurrency(price)}</div>
+        </div>
+      </div>
+    </Grid>
+  )
+}
+
+export default ItemCard;
